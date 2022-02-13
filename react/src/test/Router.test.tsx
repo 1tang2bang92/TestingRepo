a54f5f407a2b8b4dc5a8bcbd2041d9dc216b2d90
1tang2bang92 <shpark0906@naver.com>
2022-02-13 18:08:40 +0900

    테스트코드 정리

--- react/src/test/Router.test.tsx
+++ react/src/test/Router.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './Router';
+import Router from '../Router';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Router />);
+  const linkElement = screen.getByText(/friends/i);
   expect(linkElement).toBeInTheDocument();
 });
