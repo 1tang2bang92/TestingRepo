0acee2fe400b9e979126947e8b2cbe78643a5ce5
1tang2bang92 <shpark0906@naver.com>
2022-02-13 18:09:29 +0900

    nav바 추가

---
+++ react/src/components/nav.tsx
@@ -0,0 +1,16 @@
+import react from "react"
+import styled from '@emotion/styled'
+
+const Nav = () => {
+  return (
+    <NavContainer>
+    </NavContainer>
+  )
+}
+
+const NavContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`
+
+export default Nav;
