830941767d0f738241c1f5e4e39c9d3ec77148a1
1tang2bang92 <shpark0906@naver.com>
2022-02-13 18:09:19 +0900

    라우팅할 페이지 추가

--- react/src/Router.tsx
+++ react/src/Router.tsx
@@ -1,24 +1,17 @@
-import React from 'react';
+import React from 'react'
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+import Friends from './pages/Friends'
+import Chat from './pages/Chat'
 
-function App() {
+function Router() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Friends />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
-export default App;
+export default Router;
